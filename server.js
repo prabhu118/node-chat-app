0f8ee97debb27aaf94e5bfa11a08df9efc48b4eb
prabhu118 <prabodh.more@cloudesign.in>
2018-03-02 13:12:20 +0530

    Init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express               = require('express');
+const app                   = express();
+const port                  = process.env.PORT || 3000;
+const path                  = require('path');
+const publicPath            = path.join(__dirname,'public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, function(){
+    console.log(`${port}`);
+})
